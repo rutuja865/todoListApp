0bf0aed44893443cc6f9360089f1052b5e8c9969
rutuja865 <grutuja865@gmail.com>
2025-03-22 14:58:59 +0530

    changes

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 const ProtectedRoute = ({ children }) => {
   const { token } = useSelector((state) => state.auth);
   const location = useLocation();
+  console.log(children)
 
   if (!token) {
     return <Navigate to="/" state={{ from: location.pathname, message: "Please log in first." }} replace />;
